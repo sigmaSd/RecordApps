0a6bea3b5f01d701bf33f659c15a304c9c359c89
Nbiba Bedis <bedisnbiba@gmail.com>
2025-01-31 09:38:04 +0100

    unload virtual sinks at exit

--- scripts/webview.ts
+++ scripts/webview.ts
@@ -1,4 +1,5 @@
 import { Webview } from "jsr:@webview/webview@0.9.0";
+import { unloadAllVirtualSinks } from "../lib.ts";
 
 const worker = new Worker(import.meta.resolve("./start.ts"), {
   type: "module",
@@ -6,10 +7,11 @@ const worker = new Worker(import.meta.resolve("./start.ts"), {
 const webview = new Webview();
 webview.title = "Record Apps";
 
-worker.onmessage = (e) => {
+worker.onmessage = async (e) => {
   const { port } = e.data;
   webview.navigate(`http://localhost:${port}`);
   webview.run();
   worker.terminate();
+  await unloadAllVirtualSinks();
   Deno.exit(0);
 };
