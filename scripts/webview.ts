9e9b270b07af9e7b82d93780b8ace8a1cefb1276
Nbiba Bedis <bedisnbiba@gmail.com>
2025-02-01 10:49:09 +0100

    small refactor

--- scripts/webview.ts
+++ scripts/webview.ts
@@ -6,11 +6,11 @@ const worker = new Worker(import.meta.resolve("./start.ts"), {
 });
 const webview = new Webview();
 webview.title = "Record Apps";
+webview.size = { width: 1000, height: 600, hint: SizeHint.NONE };
 
 worker.onmessage = async (e) => {
   const { port } = e.data;
   webview.navigate(`http://localhost:${port}`);
-  webview.size = { width: 1000, height: 600, hint: SizeHint.NONE };
   webview.run();
   worker.terminate();
   await removeAllVirtualSinks();
